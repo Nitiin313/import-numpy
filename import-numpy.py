6ff19d709a6d81cd1920f9edd1332b254bf444de
Nitiin313 <nk9859947@gmail.com>
2026-03-11 22:41:57 +0530

    Add files via upload

---
+++ import-numpy.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Create array
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Array:", arr)
+
+# Array addition
+result = arr + 5
+print("After Adding 5:", result)
+
+# Array mean
+print("Mean:", np.mean(arr))
+
+# Array sum
+print("Sum:", np.sum(arr))
